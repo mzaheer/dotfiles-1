7e7da9c08f6424e5ba44be4e46652931ac4e97b6
Salvatore Realmuto <srealmuto@ise.com>
2014-04-22 14:12:14 -0400

    symlinks.sh: Added symlinks script
    Added symlinks script to create dotfile symlinks
    when provisioning a new workstation.

---
+++ symlinks.sh
@@ -0,0 +1,7 @@
+ln -s ~/.dotfiles/bash_profile ~/.bash_profile
+ln -s ~/.dotfiles/bashrc ~/.bashrc
+ln -s ~/.dotfiles/git-completion.bash ~/.git-completion.bash
+ln -s ~/.dotfiles/git-prompt.sh ~/.git-prompt.sh
+ln -s ~/.dotfiles/gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/vim ~/.vim
+ln -s ~/.dotfiles/vimrc ~/.vimrc
